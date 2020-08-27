7d6797ce35e3ae7be2ef29dc51933a7f06091d2d
nova <nova@signpost.eu>
2020-08-27 11:19:08 +0200

    [+] Killchance & misschance

--- commands/BalefulStrike.js
+++ commands/BalefulStrike.js
@@ -4,27 +4,52 @@ const Functions = require('../functions');
 module.exports = {
 	name: 'BalefulStrike',
 	description: 'qqqqqqqq',
-	cooldown: 5,
+	cooldown: 6,
 	aliases: ['q'],
-	args: true,
-	mention: true,
 	usage: '<@mention>',
 	// eslint-disable-next-line
 	async execute(msg, args,db) {
 		const col = db.collection('users');
-
-		const user = await Functions.CheckUserExists(msg, db);
+		const user = await Functions.getUser(msg, db);
 		if (!user) {
-			await Functions.CreateUser(msg, db);
+			await Functions.createUser(msg, db);
+		}
+
+		let target = 'nothing';
+		let incrementap = 0;
+		let kill = false;
+		let miss = false;
+		let dmgBool = false;
+		let action = 'hits';
+		const ratio = 0.08;
+		let killchance = 10 + user.ap * ratio;
+		if((10 + user.ap * ratio) > 90) { killchance = 90;}
+		const misschance = 10;
+
+		function calculateHit() {
+			target = msg.mentions.users.first();
+			if((Math.random() * 100) < misschance) miss = true;
+			if(miss) return action = 'misses';
+
+			// hit
+			dmgBool = true;
+
+			if((Math.random() * 100) < killchance) kill = true;
+			kill ? incrementap = 7 : incrementap = 1;
+			kill ? action = 'kills' : action = 'hits';
+
+		}
+
+		if(msg.mentions.users.first()) {
+			calculateHit();
 		}
 
 		// create a filter
 		const filter = { id: msg.author.id };
-
 		// create a document that increases the ap
 		const updateDoc = {
 			$set: {
-				ap: ++user.ap,
+				ap: user.ap + incrementap,
 			},
 		};
 
@@ -34,13 +59,13 @@ module.exports = {
 			`${res.matchedCount} document(s) matched the filter, updated ${res.modifiedCount} document(s)`,
 		);
 		// TODO	randomise messages
-		// TODO format
+		// TODO more efficient?
 		const exampleEmbed = new Discord.MessageEmbed()
-			.setColor('#0099ff')
+			.setColor(dmgBool ? kill ? '#FF0000' : '#0099ff' : '#000000')
 			.setTitle('Baleful Strike')
-			.setDescription(`${msg.author.username} hits ${msg.mentions.users.first()} for ${(240 + user.ap * 0.6).toFixed(0)} (60% AP) damage`)
+			.setDescription(`${msg.author.username} ${action} ${target} ${dmgBool ? `${kill ? 'with' : 'for'} **${(240 + (user.ap) * 0.6).toFixed(0)} (60% AP)** damage` : '' } and gains ${incrementap} AP ${kill ? '(5 + 2)' : ''}`)
 			.setThumbnail('https://vignette.wikia.nocookie.net/leagueoflegends/images/f/fd/Baleful_Strike.png')
-			.setFooter(`You have ${user.ap} stacks of Phenomenal Evil!`, 'https://vignette.wikia.nocookie.net/leagueoflegends/images/8/88/Phenomenal_Evil_Power.png');
+			.setFooter(`You have ${user.ap + incrementap} stacks of Phenomenal Evil!`, 'https://vignette.wikia.nocookie.net/leagueoflegends/images/8/88/Phenomenal_Evil_Power.png');
 
 		msg.channel.send(exampleEmbed);
 
